8c059eb73b8b91a16d3ff8876e9f0f22282c1e57
Zihan Kong <zihan.kong@campus.lmu.de>
2020-08-20 17:06:57 +0200

    del

--- src/mat.js
+++ src/mat.js
@@ -1,22 +1,3 @@
-/**
- * CG1 Online-Hausarbeit 3: Implementing a Rasterization Pipeline
- * Copyright (C) 2020 Changkun Ou <https://changkun.de/>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-// Note: you are not allowed to import any other APIs here.
 
 /**
  * Matrix represents a 4x4 matrix.
